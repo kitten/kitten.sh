81c2436c79a32d07377533294c476c47fc03c6ea
Phil Pluckthun <phil@kitten.sh>
2025-02-25 14:50:40 +0000

    Add redirects for old routes

---
+++ src/app/(shortlinks)/graphql-normalized-caching.tsx
@@ -0,0 +1,12 @@
+import { Redirect } from 'expo-router';
+import Head from 'expo-router/head';
+
+export default () => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <link rel="canonical" href="https://kitten.sh/blog/graphql-normalized-caching" />
+    </Head>
+    <Redirect href="/blog/graphql-normalized-caching" />
+  </>
+);
---
+++ src/app/(shortlinks)/parser-generator-dsl.tsx
@@ -0,0 +1,12 @@
+import { Redirect } from 'expo-router';
+import Head from 'expo-router/head';
+
+export default () => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <link rel="canonical" href="https://kitten.sh/blog/parser-generator-dsl" />
+    </Head>
+    <Redirect href="/blog/parser-generator-dsl" />
+  </>
+);
