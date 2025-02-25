8f0873e1b10866ed5f2cf5e3d668ff3dd3e9c03d
Phil Pluckthun <phil@kitten.sh>
2025-02-25 22:50:39 +0000

    Add missing head title to HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'expo-router';
+import Head from 'expo-router/head';
 
 import { toDateString } from '~/lib/date';
 import { metadataList } from '~/lib/posts/metadata';
@@ -6,20 +7,27 @@ import styles from './HomeScreen.module.css';
 
 export function HomeScreen() {
   return (
-    <div className={styles.main}>
-      <div className={styles.section}>
-        <h2 className={styles.heading}>Articles</h2>
-      </div>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>kitten.sh</title>
+      </Head>
 
-      <section className={styles.section}>
-        {metadataList.map((post) => (
-          <article key={post.id}>
-            <Link href={`/blog/${post.id}`} className={styles.title}>{post.title}</Link>
-            <p className={styles.subtitle}>{post.subtitle}</p>
-            <span className={styles.date}>{toDateString(post.createdAt)}</span>
-          </article>
-        ))}
-      </section>
-    </div>
+      <div className={styles.main}>
+        <div className={styles.section}>
+          <h2 className={styles.heading}>Articles</h2>
+        </div>
+
+        <section className={styles.section}>
+          {metadataList.map((post) => (
+            <article key={post.id}>
+              <Link href={`/blog/${post.id}`} className={styles.title}>{post.title}</Link>
+              <p className={styles.subtitle}>{post.subtitle}</p>
+              <span className={styles.date}>{toDateString(post.createdAt)}</span>
+            </article>
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
