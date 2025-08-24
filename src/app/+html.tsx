ed677f172b9e92234d470d1e3be81607ddfca917
Phil Pluckthun <phil@kitten.sh>
2025-08-24 18:01:34 +0100

    Add updated theme

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -6,9 +6,7 @@ export default function Root({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <link rel="preload" href="/fonts/188-70.woff2" as="font" />
-        <link rel="preload" href="/fonts/188-100.woff2" as="font" />
-        <link rel="preload" href="/fonts/departure.woff2" as="font" />
+        <link rel="preload" href="/fonts/tailrecursive-subset-latin.woff2" as="font" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="robots" content="index,follow" />
         <title>kitten.sh</title>
