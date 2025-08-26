c555dc9ae0a453a71929278b1994251a27b35f09
Phil Pluckthun <phil@kitten.sh>
2025-08-26 19:18:01 +0100

    Set publish date

--- blog/autolinkings-broken-promise/index.js
+++ blog/autolinkings-broken-promise/index.js
@@ -2,6 +2,6 @@ export const metadata = {
   title: "Autolinking's broken promise",
   subtitle: "Mending a thousand Expo apps’ dependency issues.",
   ogImage: '/og/autolinkings-broken-promise.png',
-  createdAt: '2025-10-21',
+  createdAt: '2025-08-27',
   number: 3,
 };
