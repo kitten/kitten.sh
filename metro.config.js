e532b1c436151a3adef9202f30f15492602a0be2
Phil Pluckthun <phil@kitten.sh>
2025-08-27 12:01:23 +0100

    Upgrade (54)

--- metro.config.js
+++ metro.config.js
@@ -3,16 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.transformer.getTransformOptions = async () => ({
-  transform: {
-    experimentalImportSupport: true,
-    inlineRequires: true,
-  },
-});
-
 config.transformer.babelTransformerPath = require.resolve('./src/lib/transformer.js');
 
-config.resolver.unstable_enablePackageExports = true;
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'md', 'mdx'];
 config.resolver.assetExts = [...config.resolver.assetExts, 'avif'];
 
