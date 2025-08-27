f11d1b994d6dac999be1034960037c184a0c72c0
Phil Pluckthun <phil@kitten.sh>
2025-08-27 13:07:11 +0100

    Remove preload

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -6,7 +6,6 @@ export default function Root({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <link rel="preload" href="/fonts/tailrecursive-subset-latin.woff2" as="font" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="robots" content="index,follow" />
         <title>kitten.sh</title>
