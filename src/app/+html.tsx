482835f4769fb8b1bd68b4247fd5bbffa3003b3b
Phil Pluckthun <phil@kitten.sh>
2025-09-01 20:18:05 +0100

    Add missing link rel for RSS feed

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -9,6 +9,7 @@ export default function Root({ children }: PropsWithChildren) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="robots" content="index,follow" />
         <title>kitten.sh</title>
+        <link rel="alternate" type="application/rss+xml" href="https://kitten.sh/rss.xml" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </head>
       <body>
